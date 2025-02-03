1bdb1fe5e9e78bf034ba9faa4cf094793a18b38c
Nayan Bhatia <nbhatia3@dhcp-168-37.ucsc.edu>
2025-02-03 13:50:28 -0800

    deploy html

---
+++ cron_dining_hall_data.sh
@@ -0,0 +1,180 @@
+
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=40&locationName=John+R.+Lewis+%26+College+Nine+Dining+Hall&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=40' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > johnlewis.html
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=05&locationName=Cowell+%26+Stevenson+Dining+Hall&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=40' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > stevenson.html
+
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=20&locationName=Crown+%26+Merrill+Dining+Hall+and+Banana+Joe%27s&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=05' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > crown_merill.html
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=25&locationName=Porter+%26+Kresge+Dining+Hall&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=20' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > kresge_porter.html
+
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=30&locationName=Rachel+Carson+%26+Oakes+Dining+Hall&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=25' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > rachelcarson.html
+
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=23&locationName=Oakes+Cafe&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=30' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' >oakescafe.html
+
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=46&locationName=Global+Village+Cafe&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=23' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > globalvillage.html
+
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=24&locationName=Owl%27s+Nest+Cafe&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=46' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > owlnest.html
+
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=51&locationName=Slug+Stop&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=24' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > slugstop.html
+
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=45&locationName=UCen+Bistro&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=51' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > ucbistro.html
+
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=26&locationName=Stevenson+Coffee+House&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=45' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > stevensoncoffehouse.html
+
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=22&locationName=Perk+Coffee+Bar&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=26' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > perks.html
+
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=50&locationName=Porter+Market&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=22' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > porter.html
+
+curl 'https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=47&locationName=Merrill+Market&naFlag=1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _ga=GA1.1.552647606.1691733452; _ga_B8KP5WKL0P=GS1.1.1714387165.1.0.1714387172.0.0.0; _ga_9SHY5MMRR1=GS1.1.1715115539.2.0.1715115539.0.0.0; nmstat=bfbc3cd9-dc0d-43f8-3780-fa65b0c1c0bc; _ga_QTHV5R36YZ=GS1.1.1730487203.3.0.1730487204.0.0.0; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=; _ga_BWJ4Z4Y66X=GS1.1.1738573033.10.1.1738573039.0.0.0; WebInaCartLocation=50' \
+  -H 'Referer: https://nutrition.sa.ucsc.edu/location.aspx' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1' > merillmarket.html
